bd12cf9b4d5f6158392be16e30535afda0c9fbbd
Everton Carneiro <everton.mercante@gmail.com>
2021-09-08 21:27:34 -0300

    Initial Commit

---
+++ YTSubscribeAnimation/YTSubscribeAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  YTSubscribeAnimationApp.swift
+//  YTSubscribeAnimation
+//
+//  Created by Everton Carneiro on 08/09/21.
+//
+
+import SwiftUI
+
+@main
+struct YTSubscribeAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
